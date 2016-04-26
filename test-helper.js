ff7535a4c977e8a3b7b1e896bb976a92614fafeb
Joseph McCormick <esmevane@gmail.com>
2016-04-26 16:43:24 -0500

    @wip

--- test-helper.js
+++ test-helper.js
@@ -8,9 +8,16 @@ import { createRenderer, Simulate } from 'react-addons-test-utils'
 
 chai.use(chaiImmutable)
 
+const getOutput = (component) => {
+  let renderer = createRenderer()
+  renderer.render(component)
+  return renderer.getRenderOutput()
+}
+
 if (global) {
   global.expect = chai.expect
   global.React = React
   global.Simulate = Simulate
   global.createRenderer = createRenderer
+  global.getOutput = getOutput
 }
